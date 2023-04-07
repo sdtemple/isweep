9d6d196ba05fc84a62580469a9a95ed03cd61887
sdtemple <sethdtemple@gmail.com>
2023-04-06 17:53:24 -0700

    fixed coalsimstudy py scripts

--- workflow/analyses/coalsimstudy/Neinference.py
+++ workflow/analyses/coalsimstudy/Neinference.py
@@ -38,6 +38,11 @@ ab=[long_ibd,np.inf]
 
 outfile=folder+'/'+sys.argv[7]
 
+# write to file concurrently
+
+f=open(outfile+'.Ne.tsv','w')
+f.write('PARAM\tTRUE\tINITEST\tCORREST\tCORRLOW\tCORRUPP\n')
+
 sinfs=[[] for k in range(len(lst))]
 for i in range(nreplicates):
     for k in range(len(lst)):
@@ -61,7 +66,7 @@ for i in range(nreplicates):
         ).x
         sboot=[]
         for j in range(nbootstraps):
-            print(lst[k],i,j)
+            print(lst[k],i,j) # comment out to stop stdout
             out=simulate_ibd_isweep(
                 nsamples,
                 se,
@@ -82,16 +87,74 @@ for i in range(nreplicates):
         sint=bootstrap_standard_bc(se,sboot)
         sinf=[Me,s,se,sint[1],sint[0],sint[2]]
         sinfs[k].append(sinf)
+        # saving
+        row=sinf
+        for l in range(len(row)):
+            f.write(str(row[l]))
+            if l != (len(row)-1):
+                f.write('\t')
+            else:
+                f.write('\n')
 
-with open(outfile+'.tsv','w') as f:
-    f.write('PARAM\tTRUE\tINITEST\tCORREST\tCORRLOW\tCORRUPP\n')
-    for i in range(len(sinfs)):
-        sinf=sinfs[i]
-        for j in range(len(sinf)):
-            row=sinf[j]
-            for k in range(len(row)):
-                f.write(str(row[k]))
-                if k != (len(row)-1):
-                    f.write('\t')
-                else:
-                    f.write('\n')
+f.close()
+
+
+# write to file at the end
+
+# sinfs=[[] for k in range(len(lst))]
+# for i in range(nreplicates):
+#     for k in range(len(lst)):
+#         Me=lst[k]
+#         Ne=read_Ne(Me)
+#         out=simulate_ibd_isweep(
+#             nsamples,
+#             s,
+#             p,
+#             Ne,
+#             long_ibd,
+#             long_ibd,
+#             ploidy=2
+#         )
+#         ibd=out[0][0]
+#         se = minimize_scalar(
+#             chi2_isweep,
+#             args=(p,Ne,N,(ibd,),ab),
+#             bounds=(0,0.5),
+#             method='bounded'
+#         ).x
+#         sboot=[]
+#         for j in range(nbootstraps):
+#             print(lst[k],i,j)
+#             out=simulate_ibd_isweep(
+#                 nsamples,
+#                 se,
+#                 p,
+#                 Ne,
+#                 long_ibd,
+#                 long_ibd,
+#                 ploidy=2
+#             )
+#             ibd=out[0][0]
+#             sj = minimize_scalar(
+#                 chi2_isweep,
+#                 args=(p,Ne,N,(ibd,),ab),
+#                 bounds=(0,0.5),
+#                 method='bounded'
+#             ).x
+#             sboot.append(sj)
+#         sint=bootstrap_standard_bc(se,sboot)
+#         sinf=[Me,s,se,sint[1],sint[0],sint[2]]
+#         sinfs[k].append(sinf)
+#
+# with open(outfile+'.tsv','w') as f:
+#     f.write('PARAM\tTRUE\tINITEST\tCORREST\tCORRLOW\tCORRUPP\n')
+#     for i in range(len(sinfs)):
+#         sinf=sinfs[i]
+#         for j in range(len(sinf)):
+#             row=sinf[j]
+#             for k in range(len(row)):
+#                 f.write(str(row[k]))
+#                 if k != (len(row)-1):
+#                     f.write('\t')
+#                 else:
+#                     f.write('\n')
--- workflow/analyses/coalsimstudy/sinference.py
+++ workflow/analyses/coalsimstudy/sinference.py
@@ -16,8 +16,7 @@ if not os.path.exists(folder):
 
 # fixed parameter settings
 # change selection coefficients if interested
-lst=[0.03]
-# lst=[0.02,0.03,0.04,0.05,0.06] # selection coefficients
+lst=[0.02,0.03,0.04] # selection coefficients
 
 # input parameter settings
 p=float(sys.argv[4]) # allele freq
@@ -35,6 +34,11 @@ long_ibd=3.0
 N=msamples*(msamples-1)/2-msamples
 ab=[long_ibd,np.inf]
 outfile=folder+'/'+sys.argv[7]
+
+# write to file concurrently
+
+f=open(outfile+'.s.tsv','w')
+f.write('PARAM\tTRUE\tINITEST\tCORREST\tCORRLOW\tCORRUPP\n')
 
 sinfs=[[] for k in range(len(lst))]
 for i in range(nreplicates):
@@ -58,7 +62,7 @@ for i in range(nreplicates):
         ).x
         sboot=[]
         for j in range(nbootstraps):
-            print(lst[k],i,j)
+            print(lst[k],i,j) # comment out to stop stdout
             out=simulate_ibd_isweep(
                 nsamples,
                 se,
@@ -79,16 +83,72 @@ for i in range(nreplicates):
         sint=bootstrap_standard_bc(se,sboot)
         sinf=[s,s,se,sint[1],sint[0],sint[2]]
         sinfs[k].append(sinf)
+        # saving
+        row=sinf
+        for l in range(len(row)):
+            f.write(str(row[l]))
+            if l != (len(row)-1):
+                f.write('\t')
+            else:
+                f.write('\n')
 
-with open(outfile+'.tsv','w') as f:
-    f.write('PARAM\tTRUE\tINITEST\tCORREST\tCORRLOW\tCORRUPP\n')
-    for i in range(len(sinfs)):
-        sinf=sinfs[i]
-        for j in range(len(sinf)):
-            row=sinf[j]
-            for k in range(len(row)):
-                f.write(str(row[k]))
-                if k != (len(row)-1):
-                    f.write('\t')
-                else:
-                    f.write('\n')
+f.close()
+
+# write file at the end
+
+# sinfs=[[] for k in range(len(lst))]
+# for i in range(nreplicates):
+#     for k in range(len(lst)):
+#         s=lst[k]
+#         out=simulate_ibd_isweep(
+#             nsamples,
+#             s,
+#             p,
+#             Ne,
+#             long_ibd,
+#             long_ibd,
+#             ploidy=2
+#         )
+#         ibd=out[0][0]
+#         se = minimize_scalar(
+#             chi2_isweep,
+#             args=(p,Ne,N,(ibd,),ab),
+#             bounds=(0,0.5),
+#             method='bounded'
+#         ).x
+#         sboot=[]
+#         for j in range(nbootstraps):
+#             print(lst[k],i,j)
+#             out=simulate_ibd_isweep(
+#                 nsamples,
+#                 se,
+#                 p,
+#                 Ne,
+#                 long_ibd,
+#                 long_ibd,
+#                 ploidy=2
+#             )
+#             ibd=out[0][0]
+#             sj = minimize_scalar(
+#                 chi2_isweep,
+#                 args=(p,Ne,N,(ibd,),ab),
+#                 bounds=(0,0.5),
+#                 method='bounded'
+#             ).x
+#             sboot.append(sj)
+#         sint=bootstrap_standard_bc(se,sboot)
+#         sinf=[s,s,se,sint[1],sint[0],sint[2]]
+#         sinfs[k].append(sinf)
+#
+# with open(outfile+'.tsv','w') as f:
+#     f.write('PARAM\tTRUE\tINITEST\tCORREST\tCORRLOW\tCORRUPP\n')
+#     for i in range(len(sinfs)):
+#         sinf=sinfs[i]
+#         for j in range(len(sinf)):
+#             row=sinf[j]
+#             for k in range(len(row)):
+#                 f.write(str(row[k]))
+#                 if k != (len(row)-1):
+#                     f.write('\t')
+#                 else:
+#                     f.write('\n')
