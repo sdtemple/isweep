ce68775197a1532c877068fbd5a0692339f8fe50
sdtemple <sethdtemple@gmail.com>
2023-05-10 14:37:44 -0700

    annoying edits to recombination rate plots
    bugs resolved

--- workflow/scan/terminalscripts/make-roi-table.py
+++ workflow/scan/terminalscripts/make-roi-table.py
@@ -7,7 +7,7 @@ import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-folder,mbbuffer,mapfolder=sys.argv[1]
+folder,mbbuffer,mapfolder=sys.argv[1:]
 filein=folder+'/excess.region.ibd.tsv'
 folder1=folder+'/ibdsegs/ibdends/modified/scan/'
 filepre='chr'
@@ -28,8 +28,8 @@ for i in range(roitab.shape[0]):
     rowing=roitab.iloc[i]
     filein1=folder1+filepre+str(int(float(rowing.CHROM)))+filesuf
     tab=pd.read_csv(filein1, sep='\t')
-    left=int(float(rowing.BPLEFT))-mbbuffer
-    right=int(float(rowing.BPRIGHT))+mbbuffer
+    left=int(float(rowing.BPLEFT))
+    right=int(float(rowing.BPRIGHT))
     tab=tab[(tab['BPWINDOW']>=left)&(tab['BPWINDOW']<=right)]
     tab['WEIGHT']=tab['COUNT']/tab['COUNT'].sum()
     tab['CUMSUM']=np.cumsum(tab['WEIGHT'])
@@ -55,21 +55,8 @@ for i in range(roitab.shape[0]):
     bpcenter.append(meBP)
     cmcenter.append(meCM)
 
-    # writing plots
-    chrotit=str(int(float(rowing.CHROM)))
-    plotout=folder+'/plots/chr'+ chrotit + '.mean' + str(meBP) + '.png'
-    plottit='Chromosome '+chrotit+', Mean '+str(meCM)
-    plt.plot(tab['CMWINDOW'],tab['COUNT'],linewidth=3,color='k')
-    plt.xlabel('centiMorgan distance')
-    plt.ylabel('IBD segment count')
-    plt.title(plottit)
-    plt.axvline(moCM,linestyle='dotted',linewidth=2,color='tab:gray')
-    plt.axvline(mdCM,linestyle='dashed',linewidth=2,color='tab:gray')
-    plt.axvline(meCM,linestyle='solid',linewidth=2,color='tab:gray')
-    plt.savefig(plotout,dpi=300)
-    plt.clf()
-
     # writing stats
+    chrotit=str(int(float(rowing.CHROM)))
     statout=folder+'/stats/chr'+ chrotit + '.mean' + str(meBP) + '.tsv'
     f=open(statout,'w')
     f.write('CHROM\tMODECM\tMEDIANCM\tMEANCM\tMODEBP\tMEDIANBP\tMEANBP\n')
@@ -81,6 +68,42 @@ for i in range(roitab.shape[0]):
     f.write(str(mdBP)); f.write('\t')
     f.write(str(meBP)); f.write('\n')
     f.close()
+
+    # reading
+    rowing=roitab.iloc[i]
+    filein1=folder1+filepre+str(int(float(rowing.CHROM)))+filesuf
+    tab=pd.read_csv(filein1, sep='\t')
+    left=int(float(rowing.BPLEFT))-mbbuffer
+    right=int(float(rowing.BPRIGHT))+mbbuffer
+    tab=tab[(tab['BPWINDOW']>=left)&(tab['BPWINDOW']<=right)]
+
+    # writing plots
+    chrotit=str(int(float(rowing.CHROM)))
+    plotout=folder+'/plots/chr'+ chrotit + '.mean' + str(meBP) + '.cM.png'
+    plottit='Chromosome '+chrotit+', Mean '+str(meCM)
+    plt.plot(tab['CMWINDOW'],tab['COUNT'],linewidth=3,color='k')
+    plt.xlabel('centiMorgan position')
+    plt.ylabel('IBD segment count')
+    plt.title(plottit)
+    # plt.axvline(moCM,linestyle='dotted',linewidth=2,color='tab:gray')
+    # plt.axvline(mdCM,linestyle='dashed',linewidth=2,color='tab:gray')
+    plt.axvline(meCM,linestyle='solid',linewidth=2,color='tab:gray')
+    plt.savefig(plotout,dpi=300)
+    plt.clf()
+
+    # writing plots
+    chrotit=str(int(float(rowing.CHROM)))
+    plotout=folder+'/plots/chr'+ chrotit + '.mean' + str(meBP) + '.bp.png'
+    plottit='Chromosome '+chrotit+', Mean '+str(meBP)
+    plt.plot(tab['BPWINDOW'],tab['COUNT'],linewidth=3,color='k')
+    plt.xlabel('basepair position')
+    plt.ylabel('IBD segment count')
+    plt.title(plottit)
+    # plt.axvline(moCM,linestyle='dotted',linewidth=2,color='tab:gray')
+    # plt.axvline(mdCM,linestyle='dashed',linewidth=2,color='tab:gray')
+    plt.axvline(meBP,linestyle='solid',linewidth=2,color='tab:gray')
+    plt.savefig(plotout,dpi=300)
+    plt.clf()
 
 # adding bp, cm centrality to roi table
 roitab['BPCENTER']=bpcenter
@@ -100,12 +123,15 @@ roitab.to_csv(fileout,sep='\t',index=False)
 J=roitab.shape[0]
 for j in range(J):
     row=roitab.iloc[j]
-    chrstr=str(row.CHROM.tolist()[0])
+    # chrstr=str(row.CHROM.tolist()[0])
+    chrstr=str(row.CHROM)
     mapfile=mapfolder+'/chr'+chrstr+'.map'
     genmap=pd.read_csv(mapfile,sep='\t',header=None)
-    cutleft=row.BPLEFTCENTER.tolist()[0]
-    cutright=row.BPRIGHTCENTER.tolist()[0]
-    genmapsub=genmap[(genmap[3]<cutright)&(genmap[3]>cutright)]
+    # cutleft=row.BPLEFTCENTER.tolist()[0]
+    cutleft=row.BPLEFTCENTER
+    # cutright=row.BPRIGHTCENTER.tolist()[0]
+    cutright=row.BPRIGHTCENTER
+    genmapsub=genmap[(genmap[3]<cutright)&(genmap[3]>cutleft)]
     genmapsub.reset_index(inplace=True)
     cmsdiff=[]
     bpsdiff=[]
@@ -113,17 +139,19 @@ for j in range(J):
     arr=genmapsub[2]
     arr2=genmapsub[3]
     for k in range(K):
-        cmdiff=arr[j+1]-arr[j]
-        bpdiff=arr2[j+1]-arr[j]
+        cmdiff=arr[k+1]-arr[k]
+        bpdiff=arr2[k+1]-arr[k]
         cmsdiff.append(cmdiff)
         bpsdiff.append(bpdiff)
     diffs=np.array(cmsdiff)/np.array(bpsdiff)
-    plt.plot(genmapsub[3][:-1],diffs)
+    plt.scatter(genmapsub[3][:-1],diffs,s=3)
     plt.ylabel('cM / bp')
     plt.xlabel('bp position')
-    namestr=str(row.NAME.tolist()[0])
-    chrotit=str(int(float(rowing.CHROM)))
-    meBP=str(int(float(rowing.BPCENTER)))
-    plotout=folder+'/plots/chr'+ chrotit + '.mean' + str(meBP) + '.png'
-    plt.title(namestr)
-    plt.savefig(plotout)
+    # namestr=str(row.NAME.tolist()[0])
+    namestr=str(row.NAME)
+    chrotit=str(int(float(row.CHROM)))
+    meBP=str(int(float(row.BPCENTER)))
+    plotout=folder+'/plots/chr'+ chrotit + '.mean' + str(meBP) + '.recomb.png'
+    plt.title('Chromosome '+chrotit)
+    plt.savefig(plotout,dpi=300)
+    plt.clf()
