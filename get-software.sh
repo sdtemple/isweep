2d729189cdd0f3e17370cca3ffc4a57872446a99
sdtemple <sdtemple@uw.edu>
2024-01-03 19:40:34 -0800

    small edits to docu

--- get-software.sh
+++ get-software.sh
@@ -7,7 +7,8 @@ cd $1
 # get software from the internet
 wget https://faculty.washington.edu/browning/beagle/beagle.22Jul22.46e.jar # look for the latest version
 wget https://faculty.washington.edu/browning/hap-ibd.jar
-wget https://faculty.washington.edu/browning/ibd-ends.jar 
+# wget https://faculty.washington.edu/browning/ibd-ends.jar
+# current bug; it works if you build from the git page 
 wget https://faculty.washington.edu/browning/ibdne/ibdne.23Apr20.ae9.jar
 # wget https://faculty.washington.edu/browning/beagle_utilities/filterlines.jar
 wget https://faculty.washington.edu/browning/beagle_utilities/gtstats.jar
@@ -15,6 +16,8 @@ wget https://faculty.washington.edu/browning/beagle_utilities/gtstats.jar
 # renaming some jar files
 mv ibdne.23Apr20.ae9.jar ibdne.jar
 mv beagle.22Jul22.46e.jar beagle.jar
+
+chmod 755 *.jar
 
 # go back to the original directory
 cd ..
