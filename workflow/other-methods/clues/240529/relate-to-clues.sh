4d132513b90eca44554e7d7520b35187687ac125
sdtemple <sdtemple@uw.edu>
2024-12-07 13:51:57 -0800

    removing code to run other methods. to reproduce ajhg modeling paper, see tag v1.0.

--- workflow/other-methods/clues/240529/relate-to-clues.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pyscript=$1
-mainfolder=$2
-prefix=$3
-derfile=$4
-out=$5
-python $pyscript --RelateSamples $mainfolder/$prefix.newick --DerivedFile $mainfolder/$derfile --out $mainfolder/$out
--- workflow/other-methods/clues/240529/tb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mainfolder=$1
-vcffile=$2
-tabix -fp vcf $mainfolder/$vcffile
